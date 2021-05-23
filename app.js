4a404b87321040de914894f3eef6e1da0f912a9f
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-05-23 17:48:14 +0300

    creating and editing stuff

---
+++ app.js
@@ -0,0 +1,7 @@
+let userName = prompt('what is your name?');
+let reason = prompt('what are you here for?');
+let favouriteRecipe = prompt('what is your favourite vegan recipe?');
+let veggie = prompt('what veggie do you like most?');
+
+alert('your name is ' + userName + ' you are here for ' + reason + ' favourie vegan recipe is ' + favouriteRecipe + ' most liked veggie is ' + veggie)
+console.log('your name is ' + userName + ' you are here for ' + reason + ' favourie vegan recipe is ' + favouriteRecipe + ' most liked veggie is ' + veggie)
